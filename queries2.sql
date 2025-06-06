686f7a021cba5aa17af29361b5770323cb952d4f
kuzpunk <kuzpunk@ya.ru>
2025-06-06 14:34:39 +0300

    Delete queries2.sql

--- queries2.sql
+++
@@ -1,7 +0,0 @@
-SELECT e.firstname, e.lastname, d.name AS department
-FROM humanresources.employee AS e
-JOIN humanresources.employeedepartmenthistory AS h
-ON e.businessentityid = h.businessentityid
-JOIN humanresources.department AS d
-ON h.departmentid = d.departmentid
-WHERE e.maritalstatus = 'M';
